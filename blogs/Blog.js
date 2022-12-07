9c8bcb5fc2b84482d54c854c0426338443f317bb
ecellenigma <95523566+ecellenigma@users.noreply.github.com>
2022-12-07 18:49:53 +0530

    Updated blog page
    Sri - added styles and updated the blog content

--- blogs/Blog.js
+++ blogs/Blog.js
@@ -17,33 +17,44 @@ let head = {}
 let par = {}
 let at = {}
 head[1] = "Two most important actors of Entrepreneurial Ecosystem: Accelerator & Incubator"
-par[1] = 
-`Before going in technical terms I want to start with an Example, Razor pay a fintech startup started in a small room in Jaipur by Harshil and Shashank in 2014
-They got admission in Y combinator, one of the most famous accelerator from silicon valley. They have funded them with $1.2 lakh, which made their star sings and as result of that they got funding from many investors. Now they have their head office in Bangalore with thousand’s of customers and doing wonders in fintech
-Another example is of Oyo rooms founded by Ritesh Agarwal in 2012 with lot of troubles, fortunately he got admission in Thiel fellowship of Peter Thien who was one of the key early investor in Facebook. He told Ritesh that he will give him $100 k funding and also introduce him with many of his official network so that he can grow his business. Ritesh accepted his proposal. Rest is history
-Key points about Accelerator & Incubator Accelator<br> 
-Time bounded – their purpose is to grow the startup in short span of time like in 4-12 weeks<br> 
-Clear outcome – they work with a plan and make efforts to help the founder to upgrade his startup<br> 
-Minority stake – they usually take stake in startup so that it would motivate them also to help the founder
-Incubator<br> 
-Long-time – they admit a startup for a longer time span usually more than a year <br> 
-No clear outcome – they usually fund the startup and introduce the founder with his network to upgrade his startup<br> 
-No minority stake – they usually don’t take stake in startup<br> 
-Who is better for you, accelerator or incubator ?<br> 
-<br> 
-Accelerator usually don’t take Napkin stage ideas. They fund you only when if you have already launched your product/service and you have a minimum viable product in market with a small customer engagement whereas incubator can invest in you if you have your idea in paper stage also, but you have to prove yourself and your idea such that you can solve a real world problem with ample profit to boost the valuation of business with a specific future goal<br> 
-Advantage of actuator and incubator<br> 
-Increase in valuation<br> 
-Increase in press coverage<br> 
-Change in perception<br> 
-Strengthening of network<br> 
-Saving of time <br> 
-funding<br> 
+par[1] = `INTRODUCTION<br></br>
+Before going into technical terms I want to start with an example, Razor pay a fintech startup started in a small room in Jaipur by Harshil and Shashank in 2014.
+They got admission to Y combinator, one of the most famous accelerators from silicon valley. They have funded them with $1.2 lakh, which made their star sings and as a result of that, they got funding from many investors. Now they have their head office in Bangalore with thousands of customers and doing wonders in fintech.
+Another example is Oyo rooms founded by Ritesh Agarwal in 2012 with a lot of troubles, fortunately, he got admission to the Thiel fellowship of Peter Thien who was one of the key early investors in Facebook. He told Ritesh that he will give him $100 k funding and also introduce him to many of his official networks so that he can grow his business. Ritesh accepted his proposal. Rest is history.
+<br>
+<br>
+KEY POINTS ABOUT ACCELERATOR & INCUBATOR<br></br>
+Accelerator :<br></br>
+1. Time bounded – their purpose is to grow the startup in a short period like in 4-12 weeks<br>
+2. Clear outcome – they work with a plan and make efforts to help the founder to upgrade his startup<br> 
+3. Minority stake – they usually take a stake in startups so that it would motivate them also to help the founder<br>
+<br>
+Incubator :<br></br>
+1. Long-time – they admit startups for a longer time usually more than a year<br> 
+2. No clear outcome – they usually fund the startup and introduce the founder to his networks to upgrade his startup<br>
+3. No minority stake – they usually don't take a stake in a startup<br>
+<br>
+<br>
+WHO IS BETTER FOR YOU, AN ACCELERATOR OR AN INCUBATOR?<br></br>
+Accelerators usually don't take Napkin stage ideas. They fund you only when if you have already launched your product/service and you have a minimum viable product in the market with a small customer engagement whereas an incubator can invest in you if you have your idea in the paper stage also, but you have to prove yourself and your idea such that you can solve a real-world problem with ample profit to boost the valuation of business with a specific future goal<br>
+You can click on the following link to know about top accelerators and incubators in India:<br>
+<a href="https://devx.work/blog/top-startup-accelerators-and-incubators-in-india/">Top 10 Startup Accelerators and Incubators in India - DevX</a>
 
-Most important part of incubation & acceleration program<br> 
-<br> 
-I know, You are thing that funding is the most important part of incubation & acceleration program. Trust me it is not, the most important part is Demo day<br> 
-This day comes after the graduation for the program, You are called on the stage Infront of many investors, potential customers, and people who can work with you If you impress them. You need to give a 5-10 min speech about your product/service , what kind of real life problem it is solving , what’s your future plans ,etc. and if everything will go good , personalities  Infront of you may approach you and invest in your business.`
+<br>
+<br>
+ADVANTAGES OF ACTUATOR AND INCUBATOR<br></br>
+1. Increase in valuation<br>
+2. Increase in press coverage<br>
+3. Change in perception<br>
+4. Strengthening of network<br>
+5. Saving of time<br>
+6. funding<br>
+<br>
+<br>
+<b> MOST IMPORTANT PART OF THE INCUBATION & ACCELERATION PROGRAM </b><br></br>
+I know, You think that funding is the most important part of the incubation & acceleration program. Trust me it is not, the most important part is Demo day
+This day comes after graduation from the program, You are called onto the stage in Infront of many investors, potential customers, and people who can work with you If you impress them. You need to give a 5-10 min speech about your product/service, what kind of real-life problem it is solving, what's your plans, etc. and if everything will go well, personalities  Infront of you may approach you and invest in your business.<br>
+`
 at[1] = "-Shanthanu"
 
 
