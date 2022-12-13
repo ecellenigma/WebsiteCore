0919cb0c4ca48b70525dad213ea300d18e62c3fb
ecellenigma <95523566+ecellenigma@users.noreply.github.com>
2022-12-14 00:23:52 +0530

    Added new blog - Blog 2
    Sri - added new page for the second blog

--- blogs/Blog.js
+++ blogs/Blog.js
@@ -11,56 +11,109 @@ for(i =1;i<5;i++)
     heading[i] = document.querySelector(text1.concat(i))
     para[i] = document.querySelector(text2.concat(i))
     auto[i] = document.querySelector(text3.concat(i))
+    console.log(heading[2])
 }
 
-let head = {}
-let par = {}
-let at = {}
-head[1] = "Two most important actors of Entrepreneurial Ecosystem: Accelerator & Incubator"
-par[1] = `INTRODUCTION<br></br>
-Before going into technical terms I want to start with an example, Razor pay a fintech startup started in a small room in Jaipur by Harshil and Shashank in 2014.
-They got admission to Y combinator, one of the most famous accelerators from silicon valley. They have funded them with $1.2 lakh, which made their star sings and as a result of that, they got funding from many investors. Now they have their head office in Bangalore with thousands of customers and doing wonders in fintech.
-Another example is Oyo rooms founded by Ritesh Agarwal in 2012 with a lot of troubles, fortunately, he got admission to the Thiel fellowship of Peter Thien who was one of the key early investors in Facebook. He told Ritesh that he will give him $100 k funding and also introduce him to many of his official networks so that he can grow his business. Ritesh accepted his proposal. Rest is history.
-<br>
-<br>
-KEY POINTS ABOUT ACCELERATOR & INCUBATOR<br></br>
-Accelerator :<br></br>
-1. Time bounded – their purpose is to grow the startup in a short period like in 4-12 weeks<br>
-2. Clear outcome – they work with a plan and make efforts to help the founder to upgrade his startup<br> 
-3. Minority stake – they usually take a stake in startups so that it would motivate them also to help the founder<br>
-<br>
-Incubator :<br></br>
-1. Long-time – they admit startups for a longer time usually more than a year<br> 
-2. No clear outcome – they usually fund the startup and introduce the founder to his networks to upgrade his startup<br>
-3. No minority stake – they usually don't take a stake in a startup<br>
-<br>
-<br>
-WHO IS BETTER FOR YOU, AN ACCELERATOR OR AN INCUBATOR?<br></br>
-Accelerators usually don't take Napkin stage ideas. They fund you only when if you have already launched your product/service and you have a minimum viable product in the market with a small customer engagement whereas an incubator can invest in you if you have your idea in the paper stage also, but you have to prove yourself and your idea such that you can solve a real-world problem with ample profit to boost the valuation of business with a specific future goal<br>
-You can click on the following link to know about top accelerators and incubators in India:<br>
-<a href="https://devx.work/blog/top-startup-accelerators-and-incubators-in-india/">Top 10 Startup Accelerators and Incubators in India - DevX</a>
+let head = {
+    1: "Two most important actors of Entrepreneurial Ecosystem: Accelerator & Incubator",
+    2: "How to safeguard your business from cyber attacks"
+}
+let par = {
+    1:`INTRODUCTION<br></br>
+    Before going into technical terms I want to start with an example, Razor pay a fintech startup started in a small room in Jaipur by Harshil and Shashank in 2014.
+    They got admission to Y combinator, one of the most famous accelerators from silicon valley. They have funded them with $1.2 lakh, which made their star sings and as a result of that, they got funding from many investors. Now they have their head office in Bangalore with thousands of customers and doing wonders in fintech.
+    Another example is Oyo rooms founded by Ritesh Agarwal in 2012 with a lot of troubles, fortunately, he got admission to the Thiel fellowship of Peter Thien who was one of the key early investors in Facebook. He told Ritesh that he will give him $100 k funding and also introduce him to many of his official networks so that he can grow his business. Ritesh accepted his proposal. Rest is history.
+    <br>
+    <br>
+    KEY POINTS ABOUT ACCELERATOR & INCUBATOR<br></br>
+    Accelerator :<br></br>
+    1. Time bounded – their purpose is to grow the startup in a short period like in 4-12 weeks<br>
+    2. Clear outcome – they work with a plan and make efforts to help the founder to upgrade his startup<br> 
+    3. Minority stake – they usually take a stake in startups so that it would motivate them also to help the founder<br>
+    <br>
+    Incubator :<br></br>
+    1. Long-time – they admit startups for a longer time usually more than a year<br> 
+    2. No clear outcome – they usually fund the startup and introduce the founder to his networks to upgrade his startup<br>
+    3. No minority stake – they usually don't take a stake in a startup<br>
+    <br>
+    <br>
+    WHO IS BETTER FOR YOU, AN ACCELERATOR OR AN INCUBATOR?<br></br>
+    Accelerators usually don't take Napkin stage ideas. They fund you only when if you have already launched your product/service and you have a minimum viable product in the market with a small customer engagement whereas an incubator can invest in you if you have your idea in the paper stage also, but you have to prove yourself and your idea such that you can solve a real-world problem with ample profit to boost the valuation of business with a specific future goal<br>
+    <br>
+    <br>
+    You can click on the following link to know about top accelerators and incubators in India:<br>
+    <a href="https://devx.work/blog/top-startup-accelerators-and-incubators-in-india/">Top 10 Startup Accelerators and Incubators in India - DevX</a>
+    
+    <br>
+    <br>
+    ADVANTAGES OF ACTUATOR AND INCUBATOR<br></br>
+    1. Increase in valuation<br>
+    2. Increase in press coverage<br>
+    3. Change in perception<br>
+    4. Strengthening of network<br>
+    5. Saving of time<br>
+    6. funding<br>
+    <br>
+    <br>
+    <b> MOST IMPORTANT PART OF THE INCUBATION & ACCELERATION PROGRAM </b><br></br>
+    I know, You think that funding is the most important part of the incubation & acceleration program. Trust me it is not, the most important part is Demo day
+    This day comes after graduation from the program, You are called onto the stage in Infront of many investors, potential customers, and people who can work with you If you impress them. You need to give a 5-10 min speech about your product/service, what kind of real-life problem it is solving, what's your plans, etc. and if everything will go well, personalities  Infront of you may approach you and invest in your business.<br>
+    `,
+    2:`Introduction
+    <br>
+    Just imagine that you have a business of huge valuation, plenty of funds, thousands of working staff, etc.., and in one blink everything is gone. Do you think it is impossible?.NO, it is possible. If your business is in some way linked to the internet. That’s why cyber security is needed for every business.
+    Just as bread, clothes, and houses are necessary for humans, in the same way, cyber security infrastructure is needed in business, if it is linked to the internet. The inflation of Cyberattacks in India increasing by nearly 50% in the last couple of years.
+    Cyber-attacks increased 50% Y-o-Y on corporate networks in 2021: Report - BusinessToday
+    India witnessed 18 million cyber-attacks and 200,000 threats a day in the first quarter of 2022. these are only the reported cases but the count of actual cases is much high. So cyber attack is one of the major problems to handle in the 21st century.
+    <br>
+    Types of cyber attacks
+    <br>
+    <br>
+    1. Malicious attack<br>
+    Transmission of software or any kind of file to your computer which contains a virus through any means is known as malicious attack. In a running business, it mostly it occurs via email, for example, a scammer sends you a file that contains a virus via mail with an email id similar to your friend's email id, at that instance you think that this file is sent by your friend and you download that. This may cause the loss of your data and privacy. 
+    <br><br>
+    2. Phishing attack <br>
+    Phishing attacks are the practice of sending fraudulent communications that appear to come from a reputable source. The goal is to steal sensitive data like credit card and login information, or to install malware on the victim’s machine, for example, you want to pay your college fee via the fee portal of your college, but opened a fake website that is the replica of yours college fee portal, if you pay the fee on this website your money will vanish. So whenever you open a website check that HTPPS and lock sign which means the internet protocol is secured, which is there in the URL of the website otherwise do not transact on that website. 
+    <br><br>
+    3. Identity theft<br>
+    Can you imagine a person with the same age same height same looks existing until and unless you have a twin? Most of you will tell no . but in the era of the internet everything is possible, someone can create an Id on any social or non-social platform on the internet without letting you know, this is called identity theft. People may think that they are talking to you but they are talking to a scammer, and this may cause you losses in your business 
+    <br><br>
+    4. Spoofing<br>
+    If someone or something pretends to be something else in an attempt to gain our confidence, get access to our systems, steal data, steal money, or spread malware is known as spoofing. Nowadays many things are possible beyond your imagination. For example, you can get a call from your friend or family member with the same name and number as your contact . but it may be some scammer’s call. It won't require a huge team and infrastructure. It can be done by anyone. just think that if someone contact your employ or colleague via spoofing. u can suffer a great loss.  
+    <br><br>
+    5. Attacks on wireless network<br>
+    Nowadays with government initiatives, free wifi is everywhere starting from parks to hospitals. and many private players like restaurant owners, etc, are also providing free wifi to increase the footfall in the there respective business. with the increase of free wifi, cyber attacks also increased. for example, you have connected your official computer with free wifi in a public park. you may lose your data and privacy if a hacker got a connection in your system via wireless means. So it is advised to not use public wifi, and if using it, you should not transact via that wifi. 
+    <br>
+    <br><br>
+    How to avoid cyber attacks?
+    <br>
+    1. Train your team <br>
+    Your team must know how to not be a victim of a cyber-attack, you may contact a cyber security company to take sessions and workshops.
+    <br><br>
+    Perform risk audits <br>
+    As you do a financial audit in the same way you should perform risk audit also from a genuine cyber security company to get an idea about how your company is performing in terms of security from cyber attacks. 
+    <br><br>
+    Avoid pirated softwares <br>
+    Many of you may think that, Ohh softwares are waste of money if we can get a pirated version of that for free why shouldn't we use that, trust me you are wrong, your precious data can be transferred anyware, and you can even lose your data by saving a small amount of money. with genuine software, you get security patches that keep your data secured. So looking beyond the money you should go with genuine softwares.
+    <br><br>
+    Avoid using public Wi-Fi<br>
+    One should avoid public wifi to use any type of official task, especially transactions and all. Otherwise, you may lose data, privacy, or even money.
+    <br><br>
+    Deploy antivirus software  <br>
+    You should use genuine antivirus software in your system to make the system malware-free, which will reduce the risk of loss of data and privacy 
+    <br><br>
+    Backup data on cloud <br>
+    You should have a backup of your data on genuine cloud platforms like Microsoft Azure, Google Cloud, etc. So In case of any mishappening, your data could be recovered 
+    <br><br>
+    Avoid cheap options<br>
+    Just imagine if tesla uses cheap software i.e. software developed by a newbie company that doesn’t have command on their sikills, would you like to sit in that car? Most of your answer would be no. So if you want to scale your business buy software from a genuine company otherwise you may cause software failure or data losses.`
+,
 
-<br>
-<br>
-ADVANTAGES OF ACTUATOR AND INCUBATOR<br></br>
-1. Increase in valuation<br>
-2. Increase in press coverage<br>
-3. Change in perception<br>
-4. Strengthening of network<br>
-5. Saving of time<br>
-6. funding<br>
-<br>
-<br>
-<b> MOST IMPORTANT PART OF THE INCUBATION & ACCELERATION PROGRAM </b><br></br>
-I know, You think that funding is the most important part of the incubation & acceleration program. Trust me it is not, the most important part is Demo day
-This day comes after graduation from the program, You are called onto the stage in Infront of many investors, potential customers, and people who can work with you If you impress them. You need to give a 5-10 min speech about your product/service, what kind of real-life problem it is solving, what's your plans, etc. and if everything will go well, personalities  Infront of you may approach you and invest in your business.<br>
-`
-at[1] = "-Shanthanu"
-
-
-
-
-
+}
+let at = {
+    1:"-Shanthanu",
+    2:"-Shanthanu",
+}
 
 for(i =1;i<5;i++)
 {
