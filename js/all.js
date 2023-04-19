2af234822bb5546eb1d2cc47dd8e577d7745b582
aryan <agarwalaryan139@gmail.com>
2023-04-20 02:13:30 +0530

    link update

--- js/all.js
+++ js/all.js
@@ -1700,3 +1700,28 @@ if (typeof document !== 'undefined') {
         }
     });
 })();
+
+// to ask for notification permission
+function notifyMe() {
+    // Check if the browser supports notifications
+    if (!("Notification" in window)) {
+      alert("This browser does not support desktop notification");
+    }
+  
+    // Check whether notification permissions have already been granted
+    else if (Notification.permission === "granted") {
+      // If it's okay let's create a notification
+      var notification = new Notification("Hi there!");
+    }
+  
+    // Otherwise, we need to ask the user for permission
+    else if (Notification.permission !== "denied") {
+      Notification.requestPermission().then(function (permission) {
+        // If the user accepts, let's create a notification
+        if (permission === "granted") {
+          var notification = new Notification("Hi there!");
+        }
+      });
+    }
+  }
+  
